5de3de975becdf92404e91f91d9d5a5144daea7b
Sanjay Singh <sanjay007abd@gmail.com>
2025-04-11 12:58:06 +0530

    change the path of repo in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/intellemo.ai-assignment/', // 👈 important for GitHub Pages
+});
